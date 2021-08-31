7f77c00bc5239de749da4a457574ac951b94c508
ebssgd <iamebssgd@yahoo.com>
2021-08-30 20:10:15 -0500

    Changed links to proper syntax

--- config/routes.js
+++ config/routes.js
@@ -4,6 +4,7 @@ const bcrypt = require("bcrypt");
 const jwt = require("jsonwebtoken");
 const bodyParser = require("body-parser");
 const articleController = require("../controllers/articleController");
+const { check, validationResult } = require("express-validator");
 
 module.exports = (app) => {
   app.get("/", articleController.homePage);
@@ -12,7 +13,18 @@ module.exports = (app) => {
 
   app.get("/register", articleController.register);
 
-  app.post("/register", articleController.newRegister);
+  app.post(
+    "/register",
+    check("username")
+      .notEmpty()
+      .isString()
+      .withMessage("Username can not be empty"),
+    check("password")
+      .notEmpty()
+      .isString()
+      .withMessage("Password can not be empty"),
+    articleController.newRegister
+  );
 
   app.get("/viewAll", articleController.viewAll);
 
@@ -20,13 +32,24 @@ module.exports = (app) => {
 
   app.get("/createArticle", articleController.createArticle);
 
-  app.post("/createArticle", articleController.createNewArticle);
+  app.post(
+    "/createArticle",
+    check("title")
+      .notEmpty()
+      .isLength(5)
+      .withMessage("Title must be at least 5 characters long"),
+    check("content")
+      .notEmpty()
+      .isLength(20)
+      .withMessage("Must have at least 20 characters in content"),
+    articleController.createNewArticle
+  );
 
   app.get("/editArticle", articleController.editArticle);
 
   app.get("/searchResults", articleController.searchResults);
 
-  // app.get("/logout", articleController.logout);
+  app.get("/logout", articleController.logout);
 
   app.get("/*", articleController.notFound);
 };
